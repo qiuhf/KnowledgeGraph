e4911fe3c1f54edfa7d6bcabec581302f48b5c40
qiuhaifeng <sz_qiuhf@163.com>
2021-06-28 23:01:23 +0800

    排序常见坑

---
+++ algorithm/src/main/java/com/qiuhaifeng/view/NumberPartition.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2021-2021. the original qiuhaifeng .
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.qiuhaifeng.view;
+
+/**
+ * <pre>
+ *    在整型数组中，请把奇数放在数组左边，偶数放在数组右边，要求所有奇数之间原始的相对次序不变，所有偶数之间原始相对次序不变。
+ *    要求时间复杂度做到O(N)，额外空间复杂度做到O(1)
+ * </pre>
+ *
+ * @author sz_qiuhf@163.com
+ * @since 2021-06-28
+ **/
+public class NumberPartition {
+    public static void partition(int[] arr) {
+        System.out.println("目前暂无算法可以同时做到稳定性，时间复杂度做到O(N)，额外空间复杂度做到O(1)要求");
+    }
+}
