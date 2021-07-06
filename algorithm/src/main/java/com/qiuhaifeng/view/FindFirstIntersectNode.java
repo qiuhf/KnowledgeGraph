6e4eaca343eef505a3293d130a1bb6eef1cc0446
qiuhaifeng <sz_qiuhf@163.com>
2021-07-06 11:28:10 +0800

    如果两个链表相交，请返回相交的第一个节点 如果不相交，返回null

--- algorithm/src/main/java/com/qiuhaifeng/view/FindFirstIntersectNode.java
+++ algorithm/src/main/java/com/qiuhaifeng/view/FindFirstIntersectNode.java
@@ -36,69 +36,23 @@ import java.util.Objects;
  **/
 public class FindFirstIntersectNode {
     public static void main(String[] args) {
-        int maxDepth = 10;
-        int testTime = 5;
-        Node[] nodes = generateRandomNodes(maxDepth);
+        int maxDepth = 200;
+        int testTime = 10_000;
         for (int i = 0; i < testTime; i++) {
+            Node[] nodes = generateRandomNodes(maxDepth);
             Node head1 = generateNode(nodes, maxDepth);
             Node head2 = generateNode(nodes, maxDepth);
-            Node except = useSet(head1, head2);
             Node actual = getIntersectNode(head1, head2);
-//            if (except == actual) {
-            System.out.printf("Head1: %s\nHead2: %s\n", toList(head1), toList(head2));
-            System.err.printf("Fucking! Actual: %s, Except: %s\n", (Objects.nonNull(actual) ? actual.value : null),
-                    (Objects.nonNull(except) ? except.value : null));
-//            }
+            Node except = useSet(head1, head2);
+            if (except != actual) {
+                System.out.printf("Head1: %s\nHead2: %s\n", toList(head1), toList(head2));
+                System.err.printf("Actual: %s, Except: %s\n", (Objects.nonNull(actual) ? actual.value : null),
+                        (Objects.nonNull(except) ? except.value : null));
+                return;
+            }
         }
 
         System.out.println("Nice!");
-    }
-
-    private static List toList(Node head) {
-        if (Objects.isNull(head)) {
-            return null;
-        }
-
-        List<Object> list = new ArrayList<>();
-
-        Node node = head;
-        while (Objects.nonNull(node) && !list.contains(node.value)) {
-            list.add(node.value);
-            node = node.next;
-        }
-        // 有环，记录入环节点
-        if (Objects.nonNull(node)) {
-            list.add(node.value);
-        }
-        return list;
-    }
-
-    private static Node[] generateRandomNodes(int maxLen) {
-        Node[] nodes = new Node[maxLen];
-        for (int i = 0; i < maxLen; i++) {
-            nodes[i] = new Node<>(i);
-        }
-
-        return nodes;
-    }
-
-    private static Node generateNode(Node[] nodes, int maxDepth) {
-        int left = (int) (Math.random() * maxDepth);
-        int right = Math.min((int) (Math.random() * maxDepth) + left, maxDepth);
-        if (left == 0 && right == 0) {
-            return null;
-        }
-
-        Node head = nodes[left];
-        for (int i = left + 1; i < right; i++) {
-            nodes[i - 1].next = nodes[i];
-        }
-        // 至少3个节点才形成环
-        if (Math.random() > 0.2 && (right - left) > 2) {
-            nodes[right - 1].next = nodes[(int) (Math.random() * (right - left - 2)) + left];
-        }
-
-        return head;
     }
 
     public static Node getIntersectNode(Node head1, Node head2) {
@@ -107,7 +61,7 @@ public class FindFirstIntersectNode {
         Node entry2 = getEntryNode(head2);
         // 都是无环的单链表
         if (Objects.isNull(entry1) && Objects.isNull(entry2)) {
-            return bothNoRingList(head1, head2);
+            return bothNoRingList(head1, entry1, head2, entry2);
         }
 
         // 都是有环的单链表
@@ -133,7 +87,7 @@ public class FindFirstIntersectNode {
         // 1. 第一个相交节点就是入环节点
         // 2. 第一个相交节点是入环节点之前的某个节点
         if (entry1 == entry2) {
-            return bothNoRingList(head1, head2);
+            return bothNoRingList(head1, entry1, head2, entry2);
         }
 
         // 入环节点不相同
@@ -142,8 +96,8 @@ public class FindFirstIntersectNode {
         Node cur = entry1.next;
         do {
             if (cur == entry2) {
-                // 与对数器保持一致，故此处返回entry1，实际返回entry1或entry2都可以
-                return entry1;
+                // 与对数器保持一致，故此处返回entry2，实际返回entry1或entry2都可以
+                return cur;
             }
             cur = cur.next;
             // entry1遍历一遍回到entry1节点，说明无相交点
@@ -155,13 +109,38 @@ public class FindFirstIntersectNode {
     /**
      * <p>都无环情况下，获取第一个相交节点</p>
      *
-     * @param head1 单链表1
-     * @param head2 单链表2
-     * @return <code>Node</code> null代表无相交节点
+     * @param head1  单链表1
+     * @param entry1 单链表1入环节点
+     * @param head2  单链表2
+     * @param entry2 单链表2入环节点
+     * @return <code>Node</code> 相交节点
      */
-    private static Node bothNoRingList(Node head1, Node head2) {
+    private static Node bothNoRingList(Node head1, Node entry1, Node head2, Node entry2) {
+        int len = 0;
         Node cur1 = head1;
+        while (cur1 != entry1) {
+            len++;
+            cur1 = cur1.next;
+        }
+
         Node cur2 = head2;
+        while (cur2 != entry2) {
+            len--;
+            cur2 = cur2.next;
+        }
+
+        // len < 0，head2链表长
+        // len == 0，head1和head2链表等长
+        // len > 0，head1链表长
+        cur1 = len < 0 ? head2 : head1;
+        cur2 = cur1 == head2 ? head1 : head2;
+
+        // 长链表先移动len步长
+        len = Math.abs(len);
+        while (len-- != 0) {
+            cur1 = cur1.next;
+        }
+
         // cur1和cur2等步长移动
         while (Objects.nonNull(cur1) && Objects.nonNull(cur2)) {
             // 如果节点相同，直接返回当前节点
@@ -211,31 +190,71 @@ public class FindFirstIntersectNode {
 
     public static Node useSet(Node head1, Node head2) {
         // 记录head1所有节点
-        HashSet<Node> nodeSet = new HashSet<>();
+        HashSet<Node> set1 = new HashSet<>();
         Node cur = head1;
-        while (Objects.nonNull(cur) && !nodeSet.contains(cur)) {
-            nodeSet.add(cur);
+        while (Objects.nonNull(cur) && !set1.contains(cur)) {
+            set1.add(cur);
             cur = cur.next;
         }
 
         // 遍历head2，看set是否存在相交节点
-        Node fast = Objects.nonNull(head2) ? head2.next : null;
+        HashSet<Node> set2 = new HashSet<>();
         cur = head2;
-        while (Objects.nonNull(fast)) {
-            if (nodeSet.contains(cur)) {
+        while (Objects.nonNull(cur) && !set2.contains(cur)) {
+            if (set1.contains(cur)) {
                 return cur;
             }
-            if (nodeSet.contains(fast)) {
-                return fast;
-            }
-
-            if (cur == fast) {
-                return null;
-            }
-            fast = fast.next;
+            set2.add(cur);
             cur = cur.next;
         }
 
         return null;
+    }
+
+    private static Node[] generateRandomNodes(int maxLen) {
+        Node[] nodes = new Node[maxLen];
+        for (int i = 0; i < maxLen; i++) {
+            nodes[i] = new Node<>(i);
+        }
+
+        return nodes;
+    }
+
+    private static Node generateNode(Node[] nodes, int maxDepth) {
+        int left = (int) (Math.random() * maxDepth);
+        int right = Math.min((int) (Math.random() * maxDepth) + left, maxDepth);
+        if (left == right) {
+            return null;
+        }
+
+        for (int i = left + 1; i < right; i++) {
+            if (Objects.nonNull(nodes[i].next)) {
+                return nodes[left];
+            }
+            nodes[i - 1].next = nodes[i];
+        }
+
+        // 至少3个节点才形成环
+        if (Math.random() > 0.2 && (right - left) > 2) {
+            nodes[right - 1].next = nodes[(int) (Math.random() * (right - left - 2)) + left];
+        }
+
+        return nodes[left];
+    }
+
+    private static List toList(Node head) {
+        if (Objects.isNull(head)) {
+            return null;
+        }
+
+        List<Object> list = new ArrayList<>();
+        Node node = head;
+        while (Objects.nonNull(node) && !list.contains(node.value)) {
+            list.add(node.value);
+            node = node.next;
+        }
+        // 有环，记录入环节点, 无环添加null
+        list.add(Objects.nonNull(node) ? node.value : null);
+        return list;
     }
 }
